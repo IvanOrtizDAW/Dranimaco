f1584e929a948494425e0a3ffb4caa5f8a19b981
IvanOrtizDAW <49436729+IvanOrtizDAW@users.noreply.github.com>
2020-11-24 19:58:02 +0100

    Clase Password para la encriptación de la carpeta Inglés

---
+++ Ingles/FUERA/Password.php
@@ -0,0 +1,14 @@
+
+<?php
+            
+                        class Password {
+    const SALT = 'EstoEsUnSalt';
+    public static function hash($password) {
+        return hash('sha512', self::SALT . $password);
+    }
+    public static function verify($password, $hash) {
+        return ($hash == self::hash($password));
+            }
+        }
+            
+ ?>
