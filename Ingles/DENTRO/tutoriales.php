2e77fba9b542c407f3ea46aabfbecda954b8a18b
IvanOrtizDAW <49436729+IvanOrtizDAW@users.noreply.github.com>
2020-11-30 18:58:05 +0100

    Tutoriales y sus niveles | Carpeta dentro Ingles

---
+++ Ingles/DENTRO/tutoriales.php
@@ -0,0 +1,211 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="../../Diseño/CSS/estilosDentroComunes.css" type="text/css" />
+  <link rel="stylesheet" href="../../DISEÑO/CSS/estilosTutoriales.css" type="text/css" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
+    integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous" />
+  <link rel="stylesheet" href="../../DISEÑO/Bootstrap/css/bootstrap.css" />
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+  </script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+    integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+  </script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+    integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+  </script>
+  <link rel="icon" type="image/png" href="../../Imagenes/logo2.png">
+  <title>Dranimaco Tutorials Page</title>
+</head>
+
+<body>
+  <?php
+  
+  session_start();
+  
+  if(isset($_SESSION['usuario'])){?>
+
+  <header>
+    <nav class="menu-navegacion">
+      <ul>
+        <li><img src="../../Imagenes/logo.PNG" alt="Dranimaco logo" /></li>
+        <li class="logo">DRANIMACO</li>
+
+        <?php
+
+          echo "<li class=bienvenido>Member: ".$_SESSION['usuario']."</li>";
+          
+          ?>
+
+        <li class="menu">
+          <a href="index.php"><i class="fas fa-home"></i> Home</a>
+        </li>
+        <li class="menu"><a href="historia.php"> <i class="fas fa-book"></i> History</a></li>
+        <li class="menu">
+          <a href="Galeria.php"><i class="fas fa-camera-retro"></i> Gallery</a>
+        </li>
+        <li class="menu">
+          <a href="datos.php"><i class="fas fa-globe-europe"></i> Data</a>
+        </li>
+        <li class="menu">
+          <a href="contacto.php"><i class="fas fa-phone"></i> Contact/Suggestion</a>
+        </li>
+        <li class="menu">
+          <a class="active" href="#"><i class="fas fa-play-circle"></i> Tutorials<i
+              class="fas fa-arrow-circle-down"></i></a>
+          <ul>
+            <li class="menu2">
+              <a href="basico.php"><i class="fas fa-play-circle"></i> Basic</a>
+            </li>
+            <li class="menu2">
+              <a href="medio.php"><i class="fas fa-play-circle"></i> Middle</a>
+            </li>
+            <li class="menu2">
+              <a href="experto.php"><i class="fas fa-play-circle"></i> Expert</a>
+            </li>
+          </ul>
+        </li>
+        <li class="menu">
+          <a href="../../wordpress/shop"><i class="fas fa-shopping-cart"></i> Store</a>
+        </li>
+        <li class="menu">
+          <a href="cerrarSesion.php"><i class="far fa-times-circle"></i> Sign off</a>
+        </li>
+        <li class="menu"><a href="../../Español/DENTRO/tutoriales.php"><img class="imagen-idioma"
+              src="../../Imagenes/Español.png" alt=""></a></li>
+        <li class="hamburgesa">
+          <a href="#"><i class="fas fa-bars"></i></a>
+        </li>
+      </ul>
+    </nav>
+
+  </header>
+
+  <section>
+    <div class="tutoriales">
+      <div class="card-tuto" style="width: 400px">
+        <div class="card-body">
+          <h4 class="card-title">Basic level</h4>
+          <p class="card-text">
+            Learn the most basic knowledge to get started with modeling
+            and 3D animation
+          </p>
+          <a href="basico.php" class="btn btn-info">See Tutorials</a>
+        </div>
+        <img class="card-img-bottom" src="../../Imagenes/basico.jpg" alt="Card image" style="width: 100%" />
+      </div>
+      <br />
+      <div class="card-tuto" style="width: 400px">
+        <div class="card-body">
+          <h4 class="card-title">Medium level</h4>
+          <p class="card-text">
+            After learning the basics of 3D modeling and animation
+            you can advance to the next level to improve your tech
+          </p>
+          <a href="medio.php" class="btn btn-warning">See Tutorials</a>
+        </div>
+        <img class="card-img-bottom" src="../../Imagenes/medio2.jpg" alt="Card image" style="width: 100%" />
+      </div>
+      <br />
+      <div class="card-tuto" style="width: 400px">
+        <div class="card-body">
+          <h4 class="card-title">Expert level</h4>
+          <p class="card-text">
+            After continuing to improve your modeling and animation technique
+            3D you are already suitable to carry out more ambitious projects in this
+            sector with patience and desire to continue improving
+          </p>
+          <a href="experto.php" class="btn btn-danger">See Tutorials</a>
+        </div>
+        <img class="card-img-bottom" src="../../Imagenes/experto.jpg" alt="Card image" style="width: 100%" />
+      </div>
+    </div>
+
+    <span class="ir-arriba"><a href="#"><i class="fas fa-arrow-circle-up"></i></a></span>
+
+
+  </section>
+
+  <footer>
+    <!-- Footer main -->
+    <section class="ft-main">
+      <div class="ft-main-item">
+        <h2 class="ft-title">Pages</h2>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="historia.php">History</a></li>
+          <li><a href="Galeria.php">Gallery</a></li>
+          <li><a href="datos.php">Data</a></li>
+          <li><a href="contacto.php">Contact/Suggestion</a></li>
+          <li><a href="#">Tutorials</a></li>
+          <li><a href="../../Magento">Store</a></li>
+          <li><a href="cerrarSesion.php">Sign off</a></li>
+        </ul>
+      </div>
+      <div class="ft-main-item">
+        <h2 class="ft-title">Tutorials</h2>
+        <ul>
+          <li><a href="basico.php">Basic</a></li>
+          <li><a href="medio.php">Medium</a></li>
+          <li><a href="exprto.php">Expert</a></li>
+        </ul>
+      </div>
+    </section>
+
+    <!-- Footer social -->
+    <div class="ft-social">
+      <ul class="ft-social-list">
+        <li>
+          <a href="#"><i class="fab fa-facebook"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+        </li>
+        <li>
+          <a href="#"><i class="fab fa-youtube"></i></a>
+        </li>
+      </ul>
+    </div>
+
+    <!-- Footer legal -->
+    <div class="ft-legal">
+      <ul class="ft-legal-list">
+        <li><a href="terminosCondiciones.html" target="_blank">Terms &amp; Conditions</a></li>
+        <li><a href="politicaPrivacidad.html" target="_blank">Privacy Policy</a></li>
+        <li>&copy; 2020 Copyright</li>
+      </ul>
+    </div>
+  </footer>
+
+  <script src="../../Scripts/menuHamburgesa.js"></script>
+
+  <?php 
+
+  }else{
+    echo "<div class=denegado>";
+    echo "<h1>Acceso denegado</h1>";
+    echo "<br>";
+    echo "<p>Vuelva a iniciar sesión
+    <br>
+    <a href=http://dranimaco.com/Ingles/FUERA/inicioSesion.php>Inicio de sesión</a>
+    </p> ";
+    echo "<p>Registrese si no tiene cuenta
+    <br>
+    <a href=http://dranimaco.com/Ingles/FUERA/Registro.php>Registro</a>
+    <p> ";
+    echo "<img class=denegado-img image-responsive src=../../../../Imagenes/logoDenegado.PNG>";
+    echo "</div>";
+  }
+
+?>
+
+</body>
+
+</html>
