1ab166167884ae92d43965a9700a0b3fd563420f
IvanOrtizDAW <49436729+IvanOrtizDAW@users.noreply.github.com>
2020-12-07 20:54:06 +0100

    archivo filtrado en la carpeta fuera/español

---
+++ Español/FUERA/filtrado.php
@@ -0,0 +1,9 @@
+<?php
+
+function filtrado($datos){
+    $datos = trim($datos); // Elimina espacios antes y después de los datos
+    $datos = stripslashes($datos); // Elimina backslashes \
+    $datos = htmlspecialchars($datos); // Traduce caracteres especiales en entidades HTML
+    return $datos;
+}
+?>
